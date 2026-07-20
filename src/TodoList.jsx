466875a86d2bc6cea0472b5b023c9884b5131cd0
shidoco <189675073+shidoco@users.noreply.github.com>
2026-07-19 20:32:03 -0400

    Implemented State

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,13 @@
-function TodoList() {
-    const todoList = [
-    {id: 1, title: "review resources"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-  ]
-    return (
-        <ul>{
-        todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
-        </ul>
-    );
+import TodoListItem from './TodoListItem.jsx';
+
+function TodoList({ todoList }) {
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
 }
 
 export default TodoList;
