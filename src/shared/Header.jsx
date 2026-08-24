321b8a1ccfc43e2b4f771d092dbc15600395f227
shidoco <189675073+shidoco@users.noreply.github.com>
2026-08-24 16:04:13 -0400

    edits

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,8 +1,4 @@
 function Header({ token, onSetToken, onSetEmail }) {
-  const headerToken = {token};
-  const headerSetToken = {onSetToken};
-  const headerSetEmail = {onSetEmail};
-  
   return (
     <h1>Todo List</h1>
   );
